49f00b897091a4799e2121a423ad726c61705a4f
goliluxa <goliluxa@yandex.ru>
2023-03-25 16:15:24 +0300

    jan

--- Olimp2023Cheb/29.py
+++ Olimp2023Cheb/29.py
@@ -1,17 +1,1 @@
-n = int(input())
-a = list(map(int, input().split()))
 
-k = 0
-flag1 = True
-flag2 = True
-for i in range(1, n):
-    if a[i - 1] < a[i]:
-        if flag1 or flag2:
-            k += 1
-            flag1 = False
-            if not flag1:
-                flag2 = False
-    else:
-        flag1 = True
-        flag2 = True
-print(k)
