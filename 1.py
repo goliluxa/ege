f07ae34e2b4a1b0f8839afb178da45fdc9bcf222
goliluxa <goliluxa@yandex.ru>
2023-04-12 11:16:32 +0300

    jan

--- 1.py
+++ 1.py
@@ -23,8 +23,34 @@
 #     send_email(str(random.randint(1000000, 9999999)))
 #     time.sleep(5)
 
-l = [5, 0, 1, 2, 3]
-print(l)
-print(l[:2] + l[2 + 1:])
+# l = [5, 0, 1, 2, 3]
+# print(l)
+# print(l[:2] + l[2 + 1:])
 
 # print(sorted(l))
+
+def sum_divisors(n):
+    divisors = []
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            divisors.append(i)
+            if i != n//i:
+                divisors.append(n//i)
+    if len(divisors) < 5:
+        return 0
+    divisors.sort()
+    return sum(divisors[-5:])
+
+count = 0
+n = 20000001
+while count < 5:
+    m = sum_divisors(n)
+    if m > 20000000:
+        str_m = str(m)
+        num1 = int(str_m[-3:])
+        num2 = int(str_m[-6:-3])
+        if num1+num2 > 0 and (num1+num2) % 5 == 0:
+            print(n, m)
+            count += 1
+    n += 1
+
